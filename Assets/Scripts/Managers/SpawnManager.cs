5b540f429a14a9192ac238d72850d6d63259bf1e
Kittycat <sankhayan2002@gmail.com>
2023-03-23 12:10:08 +0530

    feat: added victims

--- Assets/Scripts/Managers/SpawnManager.cs
+++ Assets/Scripts/Managers/SpawnManager.cs
@@ -4,13 +4,17 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public GameObject[] spawnPoints;
+    public GameObject[] enemySpawnPoints;
+    public GameObject[] victimSpawnPoints;
     public GameObject enemy;
+    public GameObject victim;
     // Start is called before the first frame update
     void Start()
     {
-        foreach (var spawnPoint in spawnPoints)
+        foreach (var spawnPoint in enemySpawnPoints)
             Instantiate(enemy,spawnPoint.transform.position,Quaternion.identity);
+        foreach (var spawnPoint in victimSpawnPoints)
+            Instantiate (victim,spawnPoint.transform.position,Quaternion.identity);
     }
 
     // Update is called once per frame
